5740fe844843f182ec9dfbc8e38b316a22fcd870
Nagesh <nagesh91555@gmail.com>
2024-08-13 22:56:13 +0530

    ss2

--- src/components/myimage/Myimage.jsx
+++ src/components/myimage/Myimage.jsx
@@ -1,32 +1,78 @@
-import React, { useState } from 'react';
-import {
-  SideBySideMagnifier,
-  MOUSE_ACTIVATION,
-  TOUCH_ACTIVATION
-  
-} from "react-image-magnifiers";
-// Make sure to import your CSS file
-let k=1;
-let yes=true;
-// const[flag, setflag]=useState(true);
+// import React from 'react'
+// import {
+
+//   SideBySideMagnifier,
+
+//   MOUSE_ACTIVATION,
+//   TOUCH_ACTIVATION
+// } from "react-image-magnifiers";
+// const Myimage = ({imgs}) => {
+//   return (
+//     <>
+//   <SideBySideMagnifier
+//   imageSrc={imgs}
+//   imageAlt="Example"
+//   mouseActivation={MOUSE_ACTIVATION.DOUBLE_CLICK}
+//   touchActivation={TOUCH_ACTIVATION.DOUBLE_TAP}
+//   // largeImageSrc="./large-image.jpg"
+// />
+//     <img src={imgs} alt='djcnd' height={300} width={400} className=' m-5 img-fluid shadow '/>
+//     </>
+//   )
+// }
+
+// export default Myimage
+import React from 'react';
+
+
 const MyImage = ({ imgs }) => {
   return (
     <>
-      <SideBySideMagnifier
-      
-        imageSrc={imgs}
-        imageAlt="Example"
-        mouseActivation={MOUSE_ACTIVATION.SINGLE_CLICK}
-        touchActivation={TOUCH_ACTIVATION.DOUBLE_TAP}
-        overlayBoxColor="red"
-        overlayBoxOpacity={k}
-        alwaysInPlace={yes}
-        fillAvailableSpace={yes}
-        // overlayOpacity={k}
-        className="magnifier-small " // Apply the custom class
+    
+      <img 
+        src={imgs} 
+        alt="Example" 
+        height={300} 
+        width={400} 
+        className="m-5 img-fluid shadow" 
       />
     </>
   );
 };
 
 export default MyImage;
+
+
+// import React, { useState } from 'react';
+// import {
+//   SideBySideMagnifier,
+//   MOUSE_ACTIVATION,
+//   TOUCH_ACTIVATION
+  
+// } from "react-image-magnifiers";
+// // Make sure to import your CSS file
+// let k=1;
+// let yes=true;
+// // const[flag, setflag]=useState(true);
+// const MyImage = ({ imgs }) => {
+//   return (
+//     <>
+//       <SideBySideMagnifier
+      
+//         imageSrc={imgs}
+//         imageAlt="Example"
+//         mouseActivation={MOUSE_ACTIVATION.SINGLE_CLICK}
+//         touchActivation={TOUCH_ACTIVATION.DOUBLE_TAP}
+//         overlayBoxColor="red"
+//         overlayBoxOpacity={k}
+//         alwaysInPlace={yes}
+//         fillAvailableSpace={yes}
+//         // overlayOpacity={k}
+//         className="magnifier-small " // Apply the custom class
+//       />
+//     </>
+//   );
+// };
+
+// export default MyImage;
+
